006f473caf6a5c894ad457ccca4f7836f8921ee7
Deepthi M <79050917+deepthi1107@users.noreply.github.com>
2022-07-08 20:36:44 +0530

    Add files via upload

---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1> </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default Header;
